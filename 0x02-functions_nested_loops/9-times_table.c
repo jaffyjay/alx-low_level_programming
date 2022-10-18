6e49d8fbbffc6ba6c6f9b97c6569b26904dcde5d
jaffyjay <jaffyjayprince@gmail.com>
2022-10-18 15:39:50 -0700

    Task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,7 +9,7 @@ void times_table(void)
 
 	for (num = 0; num <= 9; num++)
 	{
-		-putchar('0');
+		_putchar('0');
 
 		for (mult = 1; mult <= 9; mult++)
 		{
@@ -22,7 +22,7 @@ void times_table(void)
 				_putchar(' ');
 			else
 				_putchar((prod / 10) + '0');
-			_putchar((prod % 10) + '0'));
+			_putchar((prod % 10) + '0');
 		}
 		_putchar('\n');
 	}
