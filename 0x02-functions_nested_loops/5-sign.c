12a9ec6a9b9693cba562c9176cf702e3be761e4a
jaffyjay <jaffyjayprince@gmail.com>
2022-10-18 13:21:21 -0700

    Task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * prints_sign - prints the sign of a number
+ * print_sign - prints the sign of a number
  * @n: the number of the sign to be printed
  * Return: 1 if number is greater than zero
  * 0 of number is less than zero
