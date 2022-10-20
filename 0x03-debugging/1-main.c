e903af48717a0b39023b42448eaaf71f6892353b
jaffyjay <jaffyjayprince@gmail.com>
2022-10-20 08:33:12 -0700

    Task 1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -12,11 +12,15 @@ int main(void)
 	printf("infinite loop incoming :(\n");
 
 	i = 0;
-	/* while (i < 10)
-	 * {
-	 * putchar (i);
-	 * }
+
+	/*
+	 *while (i < 10)
+	 *{
+	 *infinite loop - no increase of variable
+	 *putchar(i);
+	 *}
 	 */
+
 	printf("infinite loop avoided \\o/\n");
 
 	return (0);
