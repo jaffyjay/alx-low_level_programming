e2c0da11e93cecbfe7bbcfd59f62fdf8e7d5e1ac
jaffyjay <jaffyjayprince@gmail.com>
2022-10-25 16:17:35 -0700

    Task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * rev_string - reverses a string
+ * @s: variable
+ */
+void rev_string(char *s)
+{
+	int a = 0;
+
+	int b = 0;
+
+	int c;
+
+	for (a = 0; s[a] != '\0'; a++)
+	{
+		continue;
+	}
+	a--;
+
+	while (a > b)
+	{
+		c = s[a];
+
+		s[a] = s[b];
+
+		s[b] = c;
+
+		b++;
+
+		a--;
+	}
+}
