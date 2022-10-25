27a1421653de407b71176c1aa5b84d342252b7bf
jaffyjay <jaffyjayprince@gmail.com>
2022-10-25 15:14:25 -0700

    Task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _puts - prints a string
+ * @str: variable
+ */
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
