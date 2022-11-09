272857a5d729f2371fc479749061dd53802c491b
jaffyjay <jaffyjayprince@gmail.com>
2022-11-09 14:04:04 -0800

    Task 5

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,66 +1,52 @@
 #include <stdio.h>
-#include "main.h"
 #include <stdlib.h>
-#include <ctype.h>
 
 /**
  * main - entry
- * @argv : the string from the terminal
- * @argc : the number of arameters
+ * @argc: holds the number
+ * @argc: the array pointer that holds the argument passed
  * Return: 0
  */
 
-int main(int argc, char const *argv[])
+int main(int argc, char *argv[])
 {
+	/*Declaring variables*/
+
+	int position, total, change, aux;
+
+	int coins[] = {25, 10, 5, 2, 1}; /*Array int*/
+
+	position = total = change = aux = 0;
+
 	if (argc != 2)
 	{
 		printf("Error\n");
-
-		return (-1);
+		return (1);
 	}
-	if (atoi(argv[1]) >= 0)
-	{
-		int c = 0;
 
-		checker(atoi(argv[1]), c);
-	}
-	else if (atoi(argv[1]) < 0)
+	total = atoi(argv[1]); /*Covert str to int*/
+
+	if (total <= 0)
 	{
 		printf("0\n");
+		return (0);
 	}
+
+	/*Declaring While*/
+
+	while (coins[position] != '\0')
+	{
+		if (total >= coins[position])
+		{
+			aux = (total / coins[position]);
+			change += aux;
+			total -= coins[position] * aux;
+		}
+
+		position++;
+
+	}
+
+	printf("%d\n", change);
 	return (0);
-}
-
-/**
- * checker - entry function
- * @c : variable
- * @b : variable
- */
-
-void checker(int c, int b)
-{
-	if (c >= 25)
-	{
-		checker(c - 25, b + 1);
-	}
-	else if (c >= 10)
-	{
-		checker(c - 10, b + 1);
-	}
-	else if (c >= 5)
-	{
-		checker(c - 5, b + 1);
-	}
-	else if (c >= 2)
-	{
-		checker(c - 2, b + 1);
-	}
-	else if (c >= 1)
-	{tom
-		checker(c - 1, b + 1);
-	}
-	else if (c == 0)
-	{
-		printf("%d\n", b);
-	}
 }
