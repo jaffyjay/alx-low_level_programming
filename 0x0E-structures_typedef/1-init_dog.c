a88591692b28c6c934aa6c5576b77db622eeb189
jaffyjay <jaffyjayprince@gmail.com>
2022-11-15 19:31:10 -0800

    Task 1

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -12,10 +12,13 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-if (d)
+if (d == NULL)
+return;
+
+else
 {
 d->name = name;
 d->age = age;
-d->owner = owner;						
+d->owner = owner;
 }
 }
